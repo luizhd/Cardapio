7a4b8d8b0009e4db8e3cc4799f0ba5a6012c331e
luizhd <luizhdernzi@hotmail.com>
2016-08-26 22:03:01 -0300

    first commit

--- www/js/app.js
+++ www/js/app.js
@@ -19,6 +19,15 @@ $('.collection')
  return false;
  });
  
+ $('#confirmar').on('click', function(){
+	 var texto = "";
+	 $('.badge').parent().each(function(){
+		 texto +=this.firstChild.textContent + ': ';
+		 texto +=this.lastChild.textContent + ', ';
+	 });
+	 $('#resumo').empty().text(texto);
+ });
+ 
  $('.acao-limpar').on('click', function() {
  $('#numero-mesa').val('');
  $('.badge').remove();
